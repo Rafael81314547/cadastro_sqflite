900b2b749ebb3ef7d392625bc2428694dcb003c4
rafael <rafael.rodrigues1@estudante.ifgoiano.edu.br>
2023-11-16 20:57:00 -0300

    formulario

---
+++ lib/contato_formulario_back.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter_application_1/contato/contato.dart';
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+part 'contato_formulario_back.g.dart';
+
+class ContatoFormularioBack = _ContatoFormularioBack with _$ContatoFormularioBack;
+
+abstract class _ContatoFormularioBack with Store{
+
+  late Contato contato;
+
+  // diferenciar novo com alteração 
+  _ContatoFormularioBack(BuildContext context){
+    var parameter = ModalRoute.of(context)?.settings.arguments;
+    if ((parameter == null)) {
+      contato = Contato();
+    } else {
+      contato = parameter as Contato;
+    }
+  }
+
+}  
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-
+import 'package:flutter_application_1/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/database_helper.dart';
+import 'package:flutter_application_1/contato/database_helper.dart';
+
+void main() => runApp(MyApp());
 
 
-void main() {
-  runApp( MyApp());
-}
-
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,80 +18,71 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatelessWidget {
+  // referencia nossa classe single para gerenciar o banco de dados
   final dbHelper = DatabaseHelper.instance;
-
+  // layout da homepage
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Exemplo de CRUD básico'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              child: Text('Inserir dados',
-                style: TextStyle(fontSize: 20),),
-              onPressed: () {_inserir();},  
+            ElevatedButton (
+              child: Text('Inserir dados', style: TextStyle(fontSize: 20),),
+              onPressed: () {_inserir();},
             ),
-            ElevatedButton(
-              child: Text('Consultar dados',
-                style: TextStyle(fontSize: 20),),
+            ElevatedButton (
+              child: Text('Consultar dados', style: TextStyle(fontSize: 20),),
               onPressed: () {_consultar();},
             ),
-            ElevatedButton(
-              child: Text('Atualizar dados',
-                style: TextStyle(fontSize: 20),),
+            ElevatedButton (
+              child: Text('Atualizar dados', style: TextStyle(fontSize: 20),),
               onPressed: () {_atualizar();},
             ),
-            ElevatedButton(
-              child: Text('Deletar dados',
-                style: TextStyle(fontSize: 20),),
+            ElevatedButton (
+              child: Text('Deletar dados', style: TextStyle(fontSize: 20),),
               onPressed: () {_deletar();},
             ),
-          ], 
+          ],
         ),
       ),
     );
   }
-  void _inserir() async{
+   // métodos dos Buttons 
+  void _inserir() async {
+    // linha para incluir
     Map<String, dynamic> row = {
       DatabaseHelper.columnNome : 'João inserir',
-      DatabaseHelper.columnIdade : 40
+      DatabaseHelper.columnIdade  : 40
     };
     final id = await dbHelper.insert(row);
     print('linha inserida id: $id');
-
   }
   void _consultar() async {
     final todasLinhas = await dbHelper.queryAllRows();
     print('Consulta todas as linhas:');
-    todasLinhas.forEach((row)=>print(row));
+    todasLinhas.forEach((row) => print(row));
   }
   void _atualizar() async {
+    // linha para atualizar
     Map<String, dynamic> row = {
-      DatabaseHelper.columnId  : 1,
+      DatabaseHelper.columnId   : 1,
       DatabaseHelper.columnNome : 'Maria atualizar',
-      DatabaseHelper.columnIdade : 32
+      DatabaseHelper.columnIdade  : 32
     };
     final linhasAfetadas = await dbHelper.update(row);
     print('atualizadas $linhasAfetadas linha(s)');
-
   }
   void _deletar() async {
+    // Assumindo que o numero de linhas é o id para a última linha
     final id = await dbHelper.queryRowCount();
     final linhaDeletada = await dbHelper.delete(id!);
-    print('Deleteda(s) $linhaDeletada linha(s): linha $id');
+    print('Deletada(s) $linhaDeletada linha(s): linha $id');
   }
 }
-
-
-
-
-
-
-
+*/
